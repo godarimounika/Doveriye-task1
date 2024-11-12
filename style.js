4fe5148a51ce6f2060110d8067fa40a8a54487f1
Godari Mounika <godarimounika02@gmail.com>
2024-11-12 16:34:56 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,7 @@
+const addToCartButton = document.querySelector('.add-to-cart');
+
+// Add a click event listener to the button
+addToCartButton.addEventListener('click', () => {
+    // Display an alert when the button is clicked
+    alert('Item added to cart!');
+});
